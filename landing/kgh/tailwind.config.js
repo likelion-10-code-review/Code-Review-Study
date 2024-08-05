d8fb4f4b41315013573e9bd37faa55ed0f61e59b
rewind122 <luvyoudear12@gmail.com>
2024-08-05 18:00:31 +0900

    마크업 및 스타일링 추가
    - tailwind prettier 설정
    - 랜딩 페이지 discover section 마크업 및 스타일링

--- landing/kgh/tailwind.config.js
+++ landing/kgh/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   theme: {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
+      pattaya: ["pattaya", "sans-serif"],
+      SquarePeg: ["Square Peg", "sans-serif"],
+      Cinzel: ["Cinzel", "serif"],
+      Vidaloka: ["Vidaloka", "serif"],
+      Farsan: ["Farsan", "cursive"],
     },
     extend: {},
   },
